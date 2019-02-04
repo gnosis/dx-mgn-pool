7212a90e8d5c718e29dd5dd15f978fdd9157a6ec
josojo <josojo@hotmail.de>
2019-02-04 23:11:11 +0100

    format fixxing

--- contracts/DxMgnPool.sol
+++ contracts/DxMgnPool.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.5.0;
 
-
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./interfaces/IDutchExchange.sol";
 
--- contracts/interfaces/IDutchExchange.sol
+++ contracts/interfaces/IDutchExchange.sol
@@ -13,11 +13,5 @@ contract IDutchExchange {
     function postBuyOrder(address token1, address token2, uint256 auctionIndex, uint256 amount) public returns(uint256);
     function postSellOrder(address token1, address token2, uint256 auctionIndex, uint256 tokensBought) public returns(uint256, uint256);
     function getCurrentAuctionPrice(address token1, address token2, uint256 auctionIndex) public view returns(uint256, uint256);
-    function claimSellerFunds(address sellToken, address buyToken, address user, uint auctionIndex)
-        public
-        returns (
-        // < (10^60, 10^61)
-        uint returned,
-        uint frtsIssued
-    );
+    function claimSellerFunds(address sellToken, address buyToken, address user, uint auctionIndex) public returns (uint returned, uint frtsIssued);
 }
