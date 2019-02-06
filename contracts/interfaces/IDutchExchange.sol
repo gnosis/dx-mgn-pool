f49af044a5fd114a0c96b78c52ee1bb5a2b7b864
Ben Smith <bh2smith@gmail.com>
2019-02-06 18:44:46 +0100

    include public view in contract to determine if canParticipate (in Auction)

--- contracts/interfaces/IDutchExchange.sol
+++ contracts/interfaces/IDutchExchange.sol
@@ -11,7 +11,7 @@ contract IDutchExchange {
     function withdraw(address tokenAddress, uint amount) public returns (uint);
     function deposit(address tokenAddress, uint amount) public returns (uint);
     function ethToken() public returns(address);
-    function getAuctionIndex(address token1, address token2) public returns(uint256);
+    function getAuctionIndex(address token1, address token2) public view returns(uint256);
     function postBuyOrder(address token1, address token2, uint256 auctionIndex, uint256 amount) public returns(uint256);
     function postSellOrder(address token1, address token2, uint256 auctionIndex, uint256 tokensBought) public returns(uint256, uint256);
     function getCurrentAuctionPrice(address token1, address token2, uint256 auctionIndex) public view returns(uint256, uint256);
