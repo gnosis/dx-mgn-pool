b82ff47421810ca1e9f320c3a6b00e20d325bfaf
Ben Smith <bh2smith@gmail.com>
2019-02-07 15:15:04 +0100

    get coordinator address from .env and include participate in auction as npm script

--- scripts/participateInAuction.js
+++ scripts/participateInAuction.js
@@ -1,19 +1,23 @@
+/* eslint-disable no-console */
 const Coordinator = artifacts.require("Coordinator")
+
+const assert = require("assert")
+
+const coordinatorAddress = process.env.CONTRACT_ADDRESS
+
+assert(coordinatorAddress, "Contract address is required")
 
 module.exports = async (callback) => {
   try {
-    const arguments = await process.argv.slice(4)
-    if (arguments.length != 1) {
-      callback("Error: This script requires arguments - <CoordinatorAddress>")
-    }
-
-    const coordinator = await Coordinator.at(arguments[0])
+    const coordinator = await Coordinator.at(coordinatorAddress)
 
     if (await coordinator.canParticipate()) {
       await coordinator.participateInAuction()
-      callback("Successfully called participateInAuction!")
+      console.log("Successfully called participateInAuction!")
+      callback()
     } else {
-      callback("Can't participat in auction yet.")  
+      console.log("Can't participat in auction yet.")
+      callback()  
     }
     
   } catch (error) {
