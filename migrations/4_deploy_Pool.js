62d388654f4d0ad95849979221d2753bac9735ee
josojo <josojo@hotmail.de>
2019-02-08 13:27:36 +0100

    fixing some small issues

--- migrations/4_deploy_Pool.js
+++ migrations/4_deploy_Pool.js
@@ -1,21 +1,22 @@
 const TRADING_PERIOD_IN_HOURS = 30*24
-let Web3 = require('web3');
-
+abi = require('ethereumjs-abi')
 module.exports = async (deployer, network, web3) => { // eslint-disable-line no-unused-vars
   const Coordinator = artifacts.require('Coordinator')
-  const DXProxy = artifacts.require('@gnosis.pm/dx-contracts/contracts/DutchExchange.sol')
-  const DXMGN = artifacts.require('@gnosis.pm/dx-contracts/contracts/TokenFRT.sol')
-  const WETH = artifacts.require('@gnosis.pm/util-contracts/contracts/EtherToken.sol')
-  const GNO = artifacts.require('@gnosis.pm/gno-token/contracts/TokenGNO.sol')
+  let DXProxy, DXMGN, WETH, GNO
 
   let EtherToken, TokenGNO, dxProxy, dxMGN;
   const contract = require('truffle-contract')
   if(network == 'development') {
+    DXProxy = artifacts.require('@gnosis.pm/dx-contracts/contracts/DutchExchangeProxy.sol')
+    DXMGN = artifacts.require('@gnosis.pm/dx-contracts/contracts/TokenFRT.sol')
+    WETH = artifacts.require('@gnosis.pm/util-contracts/contracts/EtherToken.sol')
+    GNO = artifacts.require('@gnosis.pm/gno-token/contracts/TokenGNO.sol')
+  
     etherToken = await WETH.deployed()
     tokenGNO = await GNO.deployed()
     dxProxy = await DXProxy.deployed()
     dxMGN = await DXMGN.deployed()
-  }else {
+  } else {
     DxProxy = contract(require('@gnosis.pm/dx-contracts/build/contracts/DutchExchangeProxy'))
     DxProxy.setProvider(deployer.provider)
     DxMGN = contract(require('@gnosis.pm/dx-contracts/build/contracts/TokenFRT'))
@@ -26,13 +27,13 @@ module.exports = async (deployer, network, web3) => { // eslint-disable-line no-
     TokenGNO.setProvider(deployer.provider)
     etherToken = await EtherToken.deployed()
     tokenGNO = await TokenGNO.deployed()
-    dxProxy = await DXProxy.deployed()
-    dxMGN = await DXMGN.deployed()
+    dxProxy = await DxProxy.deployed()
+    dxMGN = await DxMGN.deployed()
   }
   //To be changed to time: time is better since all the other dao stuff also depends on timestamps
   //const now = new Date()
   // const endingTradingTimestamp =  new Date(now.getTime() + TRADING_PERIOD_IN_HOURS * 60 * 60 * 1000).getTime() 
   const poolingEndBlock = 3829876 + 10000
-
+  //console.log("abi encoded constructor parameters are: ", web3.eth.abi(['address', 'address', 'address', 'address', 'uint256'], [etherToken.address, tokenGNO.address, dxMGN.address, dxProxy.address, poolingEndBlock]))
   await deployer.deploy(Coordinator, etherToken.address, tokenGNO.address, dxMGN.address, dxProxy.address, poolingEndBlock)
 }
