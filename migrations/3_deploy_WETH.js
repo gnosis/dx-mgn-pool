a2e5a9bccb44e4384f44484dae4eeab9aa8c492a
David <david.sato64@gmail.com>
2019-02-12 18:26:54 +0200

    created a separate fe_migrations folder holding migration logic
    1. allows FE to pull one migration file and use easily
    2. modularizes migration code mirroring dx-contracts migration logic
    3. cleaned up 4_deploy_pool logic

--- migrations/3_deploy_WETH.js
+++ migrations/3_deploy_WETH.js
@@ -1,19 +1,10 @@
-const deployWETH = require('@gnosis.pm/util-contracts/src/migrations-truffle-5')
+const deployWETH = require("../fe_migrations/migration_logic/migrations-truffle-5/3_deploy_WETH")
 
-
-module.exports = async (deployer, network, accounts) => {
-  if (network === 'development') {
-    const deployParams = {
-      artifacts,
-      deployer,
-      network,
-      accounts,
-      web3
-    }
-    await deployWETH(deployParams)
-  } else if (network == 'rinkeby') {
-    console.log("WETH was already deployed, skipping this migration setup")
-  } else {
-    throw new Error('Migrations are just for development. Current network is %s', network)
-  }
-}
+module.exports = async (deployer, network, accounts) =>
+  deployWETH({
+    artifacts,
+    deployer,
+    network,
+    accounts,
+    web3,
+  })
