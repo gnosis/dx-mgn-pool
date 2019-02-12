f9554cfa057db56849cde3679354f77cde589d7c
David <david.sato64@gmail.com>
2019-02-12 20:36:16 +0200

    fixed issue of TC wrap and get network

--- src/migrations/migrations-truffle-5/4_deploy_Pool.js
+++ src/migrations/migrations-truffle-5/4_deploy_Pool.js
@@ -16,9 +16,7 @@ async function migrate({
     DXMGNArtifact = artifacts.require("@gnosis.pm/dx-contracts/contracts/TokenFRT"),
     WETHArtifact = artifacts.require("@gnosis.pm/util-contracts/contracts/EtherToken"),
     GNOArtifact = artifacts.require("@gnosis.pm/gno-token/contracts/TokenGNO")
-  
-  const contractArr = [DXProxyArtifact, DXMGNArtifact, WETHArtifact, GNOArtifact]
-  
+
   // temp vars
   let etherToken, tokenGNO, dxProxy, dxMGN  
   
@@ -31,17 +29,19 @@ async function migrate({
     ]))
   } else {
     const TC = require("truffle-contract")
+    
+    const contractArtFilePaths = [
+      "@gnosis.pm/dx-contracts/build/contracts/DutchExchangeProxy.json", 
+      "@gnosis.pm/dx-contracts/build/contracts/TokenFRT.json", 
+      "@gnosis.pm/util-contracts/build/contracts/EtherToken.json", 
+      "@gnosis.pm/gno-token/build/contracts/TokenGNO.json",
+    ]
+
     // we need to setProvider to get correct networks from artifacts
-    const contractsMapped = contractArr.map(art => TC(art))
+    const contractsMapped = contractArtFilePaths.map(path => TC(require(path)))
     // Set deployer provider to each contract
     contractsMapped.forEach(tcArt => tcArt.setProvider(deployer.provider));
-    
-    ([etherToken, tokenGNO, dxProxy, dxMGN]= await Promise.all([
-      WETHArtifact.deployed(),
-      GNOArtifact.deployed(),
-      DXProxyArtifact.deployed(),
-      DXMGNArtifact.deployed(),
-    ]))
+    ([dxProxy, dxMGN, etherToken, tokenGNO] = await Promise.all(contractsMapped.map(contract => contract.deployed())))
   }
 
   const poolingTime = 3 * 60 * 60 * 24 // 3 days for testing
