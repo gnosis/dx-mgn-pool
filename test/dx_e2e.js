89d307dd4e50593e658c33bcbfd02c1498e7bcf7
josojo <josojo@hotmail.de>
2019-02-12 22:32:17 +0100

    merges master into it

--- test/dx_e2e.js
+++ test/dx_e2e.js
@@ -147,7 +147,7 @@ contract("e2e - tests", (accounts) => {
     const dx = await DX.at(dxProxy.address)
     const token_1 = await EtherToken.at(await dx.ethToken.call())
     const poolingTime = (60 * 60 * 6) + 100
-
+    const mgnToken = await TokenFRT.at(await dx.frtToken.call())
     const coordinator = await Coordinator.new(token_1.address, token_2.address, dx.address, poolingTime)
 
     const instance1 = await DxMgnPool.at(await coordinator.dxMgnPool1.call())
@@ -257,7 +257,7 @@ contract("e2e - tests", (accounts) => {
     const token_1 = await EtherToken.at(await dx.ethToken.call())
     const poolingTime = (60 * 60 * 6) + 1000000
 
-    const coordinator = await Coordinator.new(token_1.address, token_2.address, mgnToken.address, dx.address, poolingTime)
+    const coordinator = await Coordinator.new(token_1.address, token_2.address, dx.address, poolingTime)
 
     const instance1 = await DxMgnPool.at(await coordinator.dxMgnPool1.call())
     const instance2 = await DxMgnPool.at(await coordinator.dxMgnPool2.call())
@@ -401,7 +401,7 @@ contract("e2e - tests", (accounts) => {
     const token_1 = await EtherToken.at(await dx.ethToken.call())
     const poolingTime = (60 * 60 * 6) + 1000000
 
-    const coordinator = await Coordinator.new(token_1.address, token_2.address, mgnToken.address, dx.address, poolingTime)
+    const coordinator = await Coordinator.new(token_1.address, token_2.address, dx.address, poolingTime)
 
     const instance1 = await DxMgnPool.at(await coordinator.dxMgnPool1.call())
     const instance2 = await DxMgnPool.at(await coordinator.dxMgnPool2.call())
@@ -576,7 +576,7 @@ contract("e2e - tests", (accounts) => {
     const token_1 = await EtherToken.at(await dx.ethToken.call())
     const poolingTime = (60 * 60 * 6) + 1000000
 
-    const coordinator = await Coordinator.new(token_1.address, token_2.address, mgnToken.address, dx.address, poolingTime)
+    const coordinator = await Coordinator.new(token_1.address, token_2.address, dx.address, poolingTime)
 
     const instance1 = await DxMgnPool.at(await coordinator.dxMgnPool1.call())
     const instance2 = await DxMgnPool.at(await coordinator.dxMgnPool2.call())
