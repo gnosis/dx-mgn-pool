baaa8930dd3cd8e3e2b5a0b26f38452c3aaec715
Ben Smith <bh2smith@gmail.com>
2019-02-15 13:45:04 +0100

    Send email to myself on error (with error content)

--- tasks/participate.sh
+++ tasks/participate.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+MAIL_TO=ben@gnosis.pm
 
 cd /usr/src/app/
-./node_modules/.bin/truffle exec scripts/participateInAuction.js --network $NETWORK
+./node_modules/.bin/truffle exec scripts/participateInAuction.js --network $NETWORK > bot_log.txt
 
-echo "hello world" | mail -s "a subject" ben@gnosis.pm
+EXIT_STATUS=$?
+echo EXIT_STATUS
+cat bot_log.txt
+
+if [ $EXIT_STATUS -ne 0 ];then
+    echo "Sending Error message to ${MAIL_TO}"
+    cat bot_log.txt | mail -s "MGN Pool Error" ${MAIL_TO}
+fi;
