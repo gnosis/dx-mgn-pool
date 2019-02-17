52070025d91b0d19e55cafbb65ce690a11cd53d6
Ben Smith <bh2smith@gmail.com>
2019-02-17 17:13:50 +0100

    update participate in auction script to fetch gas estimates from gnosis estimator api (Need to generalize URL)

--- scripts/participateInAuction.js
+++ scripts/participateInAuction.js
@@ -1,19 +1,24 @@
 /* eslint-disable no-console */
 const Coordinator = artifacts.require("Coordinator")
+const { Gastimator } = require("./gas_station")
+
+const GasStation = new Gastimator()
+
+const url = "https://safe-relay.rinkeby.gnosis.pm/api/v1/gas-station/"
 
 module.exports = async (callback) => {
   try {
     const coordinator = await Coordinator.deployed()
     console.log("Coordinator deployed at %s", coordinator.address)
     if (await coordinator.canParticipate()) {
-      await coordinator.participateInAuction()
+      const gasPriceEstimates = await GasStation.estimateGas(url)
+      // Send transaction with fast gas price estimate
+      await coordinator.participateInAuction({ "gasPrice": gasPriceEstimates["fast"] })
       console.log("Successfully called participateInAuction!")
-      callback()
     } else {
       console.log("Can't participate in auction yet.")
-      callback()  
     }
-    
+    callback()
   } catch (error) {
     callback(error)
   }
