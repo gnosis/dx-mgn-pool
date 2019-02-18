19bd4135540b4db128f9ab1bed570779062faa77
Ben Smith <bh2smith@gmail.com>
2019-02-18 11:41:23 +0100

    try catch gas price estimate using default of 20 GWei

--- scripts/participateInAuction.js
+++ scripts/participateInAuction.js
@@ -4,17 +4,22 @@ const { Gastimator } = require("./gas_station")
 const priceUrl = require("../gas-config")
 const GasStation = new Gastimator()
 
-// TODO - make this part of configuration
-const url = priceUrl[process.env.NETWORK]
+// Use price feed based on current network (if listed) else rinkeby.
+const url = priceUrl[process.env.NETWORK] || priceUrl.rinkeby
 
 module.exports = async (callback) => {
   try {
     const coordinator = await Coordinator.deployed()
     console.log("Coordinator deployed at %s", coordinator.address)
     if (await coordinator.canParticipate()) {
-      const gasPriceEstimates = await GasStation.estimateGas(url)
+      let fastPrice = 20000000000  // 20 GWei
+      try {
+        fastPrice = (await GasStation.estimateGas(url)).fast
+      } catch (error) {
+        console.error("GasPrice etimate failed: Using default fast price of 20 GWei.")
+      }
       // Send transaction with fast gas price estimate
-      await coordinator.participateInAuction({ "gasPrice" : gasPriceEstimates.fast })
+      await coordinator.participateInAuction({ "gasPrice" : fastPrice })
       console.log("Successfully called participateInAuction!")
     } else {
       console.log("Can't participate in auction yet.")
