c97d2b41bd173a9635ea58c6766651c26c2cf283
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-03-15 15:18:53 +0100

    Trigger pool state change (#80)
    * [dev] need to clean the node_modules before install
    
    * attempt to update state of pools whenever it can.
    
    * simpler state transition logic (not checking last state change)
    
    * use dot call and put transition into try-catch
    
    * typo
    
    * don't call function unless we know its gonna work!

--- scripts/participateInAuction.js
+++ scripts/participateInAuction.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 const Coordinator = artifacts.require("Coordinator")
+const DxMgnPool = artifacts.require("DxMgnPool")
+const DutchExchange = artifacts.require("DutchExchange")
 const { Gastimator } = require("./gasStation")
 const priceUrl = require("../gas-config")
 const GasStation = new Gastimator()
@@ -24,6 +26,32 @@ module.exports = async (callback) => {
       console.log("Successfully called participateInAuction!")
     } else {
       console.log("Can't participate in auction yet.")
+      const pool1 = await DxMgnPool.at(await coordinator.dxMgnPool1())
+      const pool2 = await DxMgnPool.at(await coordinator.dxMgnPool2())
+
+      const pool1State = (await pool1.updateAndGetCurrentState.call()).toNumber()
+      const pool2State = (await pool2.updateAndGetCurrentState.call()).toNumber()
+
+      const depositToken = await pool1.depositToken()
+      const secondaryToken = await pool1.secondaryToken()
+
+      const dx1 = await DutchExchange.at(await pool1.dx())
+      const dx2 = await DutchExchange.at(await pool2.dx())
+
+      const lastAuction1 = (await pool1.lastParticipatedAuctionIndex()).toNumber()
+      const lastAuction2 = (await pool2.lastParticipatedAuctionIndex()).toNumber()
+
+      const dxAuctionIndex1 = (await dx1.getAuctionIndex.call(depositToken, secondaryToken)).toNumber()
+      const dxAuctionIndex2 = (await dx2.getAuctionIndex.call(secondaryToken, depositToken)).toNumber()
+
+      if (pool1State == 1 && dxAuctionIndex1 > lastAuction1) {
+        console.log("Pool 1: Transitioning state to `DepositWithdrawnFromDx`")
+        await pool1.triggerMGNunlockAndClaimTokens()
+      }
+      if (pool2State == 1 && dxAuctionIndex2 > lastAuction2) {
+        console.log("Pool 2: Transitioning state to `DepositWithdrawnFromDx`")
+        await pool2.triggerMGNunlockAndClaimTokens()
+      }
     }
     callback()
   } catch (error) {
