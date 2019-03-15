6fcad14e87c187a3647c403d14fb19db3e8ba63c
Ben Smith <bh2smith@gmail.com>
2019-03-15 10:45:12 +0100

    attempt to update state of pools whenever it can.

--- scripts/participateInAuction.js
+++ scripts/participateInAuction.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const Coordinator = artifacts.require("Coordinator")
+const DxMgnPool = artifacts.require("DxMgnPool")
 const { Gastimator } = require("./gasStation")
 const priceUrl = require("../gas-config")
 const GasStation = new Gastimator()
@@ -24,6 +25,20 @@ module.exports = async (callback) => {
       console.log("Successfully called participateInAuction!")
     } else {
       console.log("Can't participate in auction yet.")
+      const pool1 = await DxMgnPool.at(await coordinator.dxMgnPool1())
+      const pool2 = await DxMgnPool.at(await coordinator.dxMgnPool2())
+
+      const pool1State = (await pool1.currentState()).toNumber()
+      if (pool1State == 1) {
+        await pool1.triggerMGNunlockAndClaimTokens()
+        await pool2.triggerMGNunlockAndClaimTokens()
+      } else if (pool1State == 2) {
+        await pool1.withdrawUnlockedMagnoliaFromDx()
+        await pool2.withdrawUnlockedMagnoliaFromDx()
+      }
+      if ((await pool1.currentState()).toNumber() > pool1State) {
+        console.log("Pooling state transitioned from {}", pool1State)
+      }
     }
     callback()
   } catch (error) {
